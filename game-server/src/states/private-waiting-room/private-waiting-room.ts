edb70c808d5fc79fb8167c65440c84196f9b4a54
Nikhil Simhambhatla <nsimhamb@gmail.com>
2024-08-28 14:52:04 -0700

    Added relay-service to private-waiting-room

--- game-server/src/states/private-waiting-room/private-waiting-room.ts
+++ game-server/src/states/private-waiting-room/private-waiting-room.ts
@@ -1,13 +1,16 @@
 import type PlayerDB from "../../player-db/player-db"
+import type RelayService from "../../relay-service/relay-service"
 import { State } from "../types"
 
 export default class PrivateWaitingRoom {
   stateMap: Map<string, State>
   playerDB: PlayerDB
+  relayService: RelayService
 
-  constructor(stateMap: Map<string, State>, playerDB: PlayerDB) {
+  constructor(stateMap: Map<string, State>, playerDB: PlayerDB, relayService: RelayService) {
     this.stateMap = stateMap;
     this.playerDB = playerDB;
+    this.relayService = relayService;
   }
   
 }
