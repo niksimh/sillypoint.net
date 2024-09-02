4a253edaf451e7563e653af3bc2b5d53d070aac8
Nikhil Simhambhatla <nsimhamb@gmail.com>
2024-09-02 10:11:16 -0700

    Added pendingConnection client state when at /game

---
+++ web-client/src/app/game/gameStates/pending.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/shared/Footer";
+import Header from "@/shared/Header";
+
+export default function PendingState() {
+  return (
+    <main>
+      <h1>Connecting to game servers</h1>
+      <h2 className="loading"></h2>
+    </main>
+  );
+}
--- web-client/src/app/game/page.tsx
+++ web-client/src/app/game/page.tsx
@@ -1,3 +1,27 @@
+'use client';
+
+import Footer from "@/shared/Footer";
+import Header from "@/shared/Header";
+import PendingState from "./gameStates/pending";
+
+import { useState } from "react";
+
 export default function GamePage() {
-  return <h1>Game stub</h1>
+  
+  let [gameState, setGameState] = useState("pendingConnection");
+  
+  let renderedGameState;
+  switch(gameState) {
+    case "pendingConnection": 
+      renderedGameState = <PendingState />
+      break;
+  }
+
+  return (
+    <>
+      <Header title="Game"/>
+      {renderedGameState}
+      <Footer />
+    </>
+  );
 }
