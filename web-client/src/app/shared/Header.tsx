0dad3d33d1e2a18d7663f9c46e897f8d42eb60da
Nikhil Simhambhatla <nsimhamb@gmail.com>
2024-09-10 11:36:10 -0700

    Completed /rules styling

--- web-client/src/app/shared/Header.tsx
+++ web-client/src/app/shared/Header.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 export default function Header({ title }: { title: string } ){
   return (
-    <header>
-      <h1>
-        <Link href='/'>SP</Link> ||| { title }</h1>
+    <header className="h-[10dvh] ls:h-[15dvh] pt-4 pl-4">
+      <h1 className="white font-bold text-3xl sm:text-4xl ls:text-2xl">
+        <Link className="pink hover:underline"href='/'>SP</Link> ||| { title }
+      </h1>
     </header>
   );
 }
