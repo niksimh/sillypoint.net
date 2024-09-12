8bea882c0d12ceae2b6f5f87f729d788b7584f7f
Nikhil Simhambhatla <nsimhamb@gmail.com>
2024-09-11 20:25:34 -0700

    Updated styling at pending and public waiting room

--- web-client/src/app/game/gameStates/pending.tsx
+++ web-client/src/app/game/gameStates/pending.tsx
@@ -1,7 +1,7 @@
 export default function PendingState() {
   return (
-    <main className="h-[80dvh] ls:h-[75dvh] flex flex-col justify-center items-center px-12">
-      <h1 className="font-bold text-xl sm:text-2xl ls:text-lg">Connecting to game servers</h1>
+    <main className="h-[80dvh] ls:h-[75dvh] flex flex-col justify-center items-center">
+      <h1 className="font-bold text-center text-xl sm:text-2xl ls:text-lg">Connecting to game servers</h1>
       <h2 className="loading font-bold text-xl sm:text-2xl ls:text-lg"></h2>
     </main>
   );
--- web-client/src/app/game/gameStates/publicWaitingRoom.tsx
+++ web-client/src/app/game/gameStates/publicWaitingRoom.tsx
@@ -1,12 +1,10 @@
 'use client'
 
-import { GameInput } from "@/types/io-types";
-
 export function PublicWaitingRoomState() {
   
   return (
-    <main className="h-[80dvh] ls:h-[75dvh] flex flex-col justify-center items-center px-12">
-      <h1 className="font-bold text-xl sm:text-2xl ls:text-lg">Searching for a game!</h1>
+    <main className="h-[80dvh] ls:h-[75dvh] flex flex-col justify-center items-center">
+      <h1 className="font-bold text-center text-xl sm:text-2xl ls:text-lg">Searching for a game!</h1>
       <h2 className="loading font-bold text-xl sm:text-2xl ls:text-lg"></h2>
     </main>
   )
