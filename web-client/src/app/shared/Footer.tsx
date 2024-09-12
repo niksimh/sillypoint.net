35b5e5778871fa561b670eab7f8b0b3130f07f9a
Nikhil Simhambhatla <nsimhamb@gmail.com>
2024-09-12 00:29:14 -0700

    Removed contact from footer

--- web-client/src/app/shared/Footer.tsx
+++ web-client/src/app/shared/Footer.tsx
@@ -3,12 +3,6 @@ export default function Footer() {
     <footer>
       <ul className="h-[10dvh] flex justify-center items-end gap-2 pb-1">
         <li>&copy; sillypoint.net</li>
-        <li>|</li>
-        <li>
-          <address>
-            <a href="mailto:#">Contact Us!</a>
-          </address>
-        </li>
       </ul>
     </footer>
   );
