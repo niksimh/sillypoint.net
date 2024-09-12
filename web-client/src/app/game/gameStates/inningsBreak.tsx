24da7a9069831f9c11534f6100e9ca78dead7ff4
Nikhil Simhambhatla <nsimhamb@gmail.com>
2024-09-11 17:32:21 -0700

    Grammar fix in innings break

--- web-client/src/app/game/gameStates/inningsBreak.tsx
+++ web-client/src/app/game/gameStates/inningsBreak.tsx
@@ -8,12 +8,12 @@ export function InningsBreakState(
     try {
       let playerIdTokenPayload = jwtDecode(localStorage.getItem("playerIdToken")!) as PlayerIdTokenPayload;
       if(gameStateData.nextBatterId === playerIdTokenPayload.playerId) {
-        nextBatterUsername = "You";
+        nextBatterUsername = "You are";
       } else {
         if(gameStateData.p1.playerId === gameStateData.nextBatterId) {
-          nextBatterUsername = gameStateData.p1.username; 
+          nextBatterUsername = gameStateData.p1.username+" is"; 
         } else {
-          nextBatterUsername = gameStateData.p2.username; 
+          nextBatterUsername = gameStateData.p2.username+" is"; 
         }
       }
     } catch {
@@ -22,7 +22,7 @@ export function InningsBreakState(
     
     return (
     <main className="h-[80dvh] ls:h-[75dvh] flex flex-col justify-center items-center gap-10 ls:gap- px-12">      
-      <h1 className="pink font-bold text-xl sm:text-2xl ls:text-lg">{`${nextBatterUsername} is batting next.`}</h1>
+      <h1 className="pink font-bold text-xl sm:text-2xl ls:text-lg">{`${nextBatterUsername} batting next.`}</h1>
       <h1 className="font-bold text-xl sm:text-2xl ls:text-lg">{`${gameStateData.target} is needed to win.`}</h1>
       <h1 className="pink font-bold text-xl sm:text-2xl ls:text-lg">Innings 2 will start shortly!</h1>
     </main>
